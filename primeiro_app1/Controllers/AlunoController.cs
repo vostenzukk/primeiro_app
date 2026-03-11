6ee142d2eaf29088b5a06939a08e4c46d3a6c617
vostenzukk <marcosfelicianob@gmail.com>
2026-03-11 17:18:48 -0300

    Adicionando Projeto

---
+++ primeiro_app1/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace primeiro_app.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ primeiro_app1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace primeiro_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
